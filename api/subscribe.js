4f954c0789d866b66194dfe644ce2b5a2e6271d5
Gentx <gentx@ordinatedegentx.home>
2026-05-12 01:15:28 +0200

    Fix Brevo attribute name FIRSTNAME -> PRENOM

--- api/subscribe.js
+++ api/subscribe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     },
     body: JSON.stringify({
       email,
-      attributes: { FIRSTNAME: prenom || '' },
+      attributes: { PRENOM: prenom || '' },
       listIds: [listId],
       updateEnabled: true
     })
